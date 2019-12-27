73cce4c47c34061e372028a2b397d50cf9d86e52
Hongzhi Li <hongzhi.li@live.com>
2019-12-27 14:00:45 +0000

    update

--- setup_lustre.sh
+++ setup_lustre.sh
@@ -19,16 +19,16 @@ EOM
 
 
 yum install -y e2fsprogs
+mkdir /tmp/lustre
+wget -O /tmp/lustre/kernel-3.10.0-1062.1.1.el7_lustre.x86_64.rpm https://downloads.whamcloud.com/public/lustre/lustre-2.13.0/el7.7.1908/server/RPMS/x86_64/kernel-3.10.0-1062.1.1.el7_lustre.x86_64.rpm
+wget -O /tmp/lustre/lustre-2.13.0-1.el7.x86_64.rpm https://downloads.whamcloud.com/public/lustre/lustre-2.13.0/el7.7.1908/server/RPMS/x86_64/lustre-2.13.0-1.el7.x86_64.rpm
+wget -O /tmp/lustre/kmod-lustre-2.13.0-1.el7.x86_64.rpm https://downloads.whamcloud.com/public/lustre/lustre-2.13.0/el7.7.1908/server/RPMS/x86_64/kmod-lustre-2.13.0-1.el7.x86_64.rpm
+wget -O /tmp/lustre/kmod-lustre-osd-ldiskfs-2.13.0-1.el7.x86_64.rpm https://downloads.whamcloud.com/public/lustre/lustre-2.13.0/el7.7.1908/server/RPMS/x86_64/kmod-lustre-osd-ldiskfs-2.13.0-1.el7.x86_64.rpm
+wget -O /tmp/lustre/lustre-osd-ldiskfs-mount-2.13.0-1.el7.x86_64.rpm https://downloads.whamcloud.com/public/lustre/lustre-2.13.0/el7.7.1908/server/RPMS/x86_64/lustre-osd-ldiskfs-mount-2.13.0-1.el7.x86_64.rpm
+wget -O /tmp/lustre/lustre-tests-2.13.0-1.el7.x86_64.rpm https://downloads.whamcloud.com/public/lustre/lustre-2.13.0/el7.7.1908/server/RPMS/x86_64/lustre-tests-2.13.0-1.el7.x86_64.rpm
 
-wget https://downloads.whamcloud.com/public/lustre/lustre-2.13.0/el7.7.1908/server/RPMS/x86_64/kernel-3.10.0-1062.1.1.el7_lustre.x86_64.rpm
-wget https://downloads.whamcloud.com/public/lustre/lustre-2.13.0/el7.7.1908/server/RPMS/x86_64/lustre-2.13.0-1.el7.x86_64.rpm
-wget https://downloads.whamcloud.com/public/lustre/lustre-2.13.0/el7.7.1908/server/RPMS/x86_64/kmod-lustre-2.13.0-1.el7.x86_64.rpm
-wget https://downloads.whamcloud.com/public/lustre/lustre-2.13.0/el7.7.1908/server/RPMS/x86_64/kmod-lustre-osd-ldiskfs-2.13.0-1.el7.x86_64.rpm
-wget https://downloads.whamcloud.com/public/lustre/lustre-2.13.0/el7.7.1908/server/RPMS/x86_64/lustre-osd-ldiskfs-mount-2.13.0-1.el7.x86_64.rpm
-wget https://downloads.whamcloud.com/public/lustre/lustre-2.13.0/el7.7.1908/server/RPMS/x86_64/lustre-tests-2.13.0-1.el7.x86_64.rpm
-
-yum install -y kernel-3.10.0-1062.1.1.el7_lustre.x86_64.rpm
-yum install -y kmod*
-yum install -y lustre*
+yum install -y /tmp/lustre/kernel-3.10.0-1062.1.1.el7_lustre.x86_64.rpm
+yum install -y /tmp/lustre/kmod*
+yum install -y /tmp/lustre/lustre*
 
 
